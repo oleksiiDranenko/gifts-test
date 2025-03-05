833f26c027458aaf5b51af64c882dbd7d0b50595
oleksiiDranenko <alexdranenko2004@icloud.com>
2025-03-05 18:16:47 +0000

    test

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,21 @@
+'use client'
 
 import GiftChart from '@/components/GIftChart'
 import { data } from '@/data'
 
+import WebApp from '@twa-dev/sdk'
+import { useEffect } from 'react'
+
 export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-row justify-center pt-14">
-      <GiftChart list={data}/>
-    </main>
-  )
+
+	useEffect(() => {
+		WebApp.ready();  // Initialize Telegram Web App
+		WebApp.expand(); // Prevent swipe-down close
+	}, []);
+
+  	return (
+    	<main className="flex min-h-screen flex-row justify-center pt-14">
+      		<GiftChart list={data}/>
+    	</main>
+  	)
 }
