f5ff76e6b97d25e9896533ebc1bb52b8023329bd
oleksiiDranenko <alexdranenko2004@icloud.com>
2025-03-05 18:38:12 +0000

    test

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,20 +2,33 @@
 
 import GiftChart from '@/components/GIftChart'
 import { data } from '@/data'
+import { useEffect, useState } from 'react'
 
-import WebApp from '@twa-dev/sdk'
-import { useEffect } from 'react'
-
+// Client-side only logic, avoid SSR errors
 export default function Home() {
+  const [isClient, setIsClient] = useState(false);
 
-	useEffect(() => {
-		WebApp.ready();  // Initialize Telegram Web App
-		WebApp.expand(); // Prevent swipe-down close
-	}, []);
+  useEffect(() => {
+    // Ensuring this runs only on the client side (in the browser)
+    setIsClient(true);
+  }, []);
 
-  	return (
-    	<main className="flex min-h-screen flex-row justify-center pt-14">
-      		<GiftChart list={data}/>
-    	</main>
-  	)
+  // We can import WebApp inside useEffect to ensure it runs only on the client-side
+  useEffect(() => {
+    if (isClient) {
+      import('@twa-dev/sdk').then((WebApp) => {
+        // Type assertion to tell TypeScript that WebApp has ready and expand methods
+        (WebApp as any).ready();  // Initialize Telegram Web App
+        (WebApp as any).expand(); // Prevent swipe-down close
+      }).catch((err) => {
+        console.error('Error loading WebApp SDK:', err);
+      });
+    }
+  }, [isClient]);
+
+  return (
+    <main className="flex min-h-screen flex-row justify-center pt-14">
+      <GiftChart list={data} />
+    </main>
+  );
 }
